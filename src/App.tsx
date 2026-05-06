996d04c54c529bafbaffca27b009796843213aac
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-05-05 23:21:18 -0300

    Heading component created and added some global css configurations to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Heading } from './components/Heading';
+import './styles/global.css';
+import './styles/theme.css';
+import { TimerIcon } from 'lucide-react';
+
 export function App()
 {
     return (
         <div>
-            <h1>Hello React!</h1>
+            <Heading>
+                Hello, React!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea cumque nemo laborum eum repellendus! Odio, adipisci consequuntur, soluta veniam fugit, cupiditate temporibus quasi dolorem ipsum non inventore ullam vel eos.
             </p>
